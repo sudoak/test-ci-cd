97004014e989da8771b4a3cbe922fe0a36193356
sudoak <akram@coffeebeans.io>
2020-10-22 16:34:54 +0530

    refactor: added response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send(`Hello World! ${Date.now()}`)
+  res.send(`Hello My World! ${Date.now()}`)
 })
 
 app.listen(port, () => {
